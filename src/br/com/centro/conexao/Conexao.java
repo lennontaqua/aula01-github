963d52ddecd453fe324a1c52989942cb88dffac0
lennontaqua <lennontaqua@hotmail.com>
2019-02-17 15:59:16 -0300

    Mudei

--- src/br/com/centro/conexao/Conexao.java
+++ src/br/com/centro/conexao/Conexao.java
@@ -10,6 +10,7 @@ public class Conexao {
     
     public static EntityManager getCreatConnection (){
         EntityManagerFactory em = Persistence.createEntityManagerFactory("PrjEtec");
+        System.out.println("Muito fera mano");
         return em.createEntityManager();
     
     }
